1f4844bd77d99996b7d1094bbe10754cb6edf0f6
VagTsop <vatsop52@gmail.com>
2021-10-22 20:40:19 +0300

    implement tabbed component

--- public/script.js
+++ public/script.js
@@ -44,3 +44,30 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
 });
+
+// Tabbed component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+//tabs.forEach(t => t.addEventListener('click', () => console.log('TAB')));
+
+// Attach event handler to parent element with event delegation
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab'); // use closest method in order to get the parent element of span which is the button // now wherever we click the button we get the same element even though we click on button or on span because it finds the closest parent element with this class name
+
+  // Guard clause
+  if (!clicked) return;
+
+  // remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  // Activate Tab
+  clicked.classList.add('operations__tab--active');
+
+  // Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
